c2058f48849a4d0a75677266cf01ca8544e14821
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2024-06-30 19:51:28 -0300

    feat: initial commit

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  try {
+    const supabase = createMiddlewareClient({ req, res });
+    await supabase.auth.getSession();
+  } catch (e) {
+    console.error("middleware", e);
+  }
+  return res;
+}
